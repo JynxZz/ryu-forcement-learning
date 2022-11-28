6ee91d0308287f0349f06f7c1dc03941acefc9e9
Adrien Cellario <adriencellario@gmail.com>
2022-11-28 15:04:00 +0100

    final setup

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
 # DIAMBRA Arena module import
 import diambra.arena
-from IPython import display
+from diambra.arena.utils.gym_utils import show_gym_obs
 
 # Environment creation
 env = diambra.arena.make("sfiii3n")
 
 # Environment reset
 observation = env.reset()
+show_gym_obs(observation=observation, n_actions_stack=env.n_act_stack, char_list=env.char_names, wait_key=1, viz=True)
+
 
 # Agent-Environment interaction loop
 while True:
@@ -15,7 +17,6 @@ while True:
 
     # Action random sampling
     actions = env.action_space.sample()
-
     # Environment stepping
     observation, reward, done, info = env.step(actions)
 
