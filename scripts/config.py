7f6c30a9f203a3e4e82bdc9c1a078ac19c474359
corentincadene <corentincadene@gmail.com>
2022-11-29 15:01:34 +0100

    Adrien push request

--- scripts/config.py
+++ scripts/config.py
@@ -39,47 +39,46 @@ wrappers_settings = {
   }
 
 
-obs= {
-    'observations': {
-    'P1_actions_attack': [torch.tensor([[0.],[8.],[2.],[7.],[5.]])],
-    'P1_actions_move': [torch.tensor([[0.],[6.],[1.],[1.],[4.]])],
-    'P1_oppChar': [torch.tensor([[9.],[9.],[9.],[9.],[9.]])],
-    'P1_oppChar1': [torch.tensor([[9.],[9.],[9.],[9.],[9.]])],
-    'P1_oppHealth': [torch.tensor([[160.],[160.],[160.],[160.],[160.]])],
-    'P1_oppSide': [torch.tensor([[1.],[1.],[1.],[1.],[1.]])],
-    'P1_oppStunBar': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_oppStunned': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_oppSuperBar': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_oppSuperCount': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_oppSuperMaxCount': [torch.tensor([[1.],[1.],[1.],[1.],[1.]])],
-    'P1_oppSuperType': [torch.tensor([[1.],[1.],[1.],[1.],[1.]])],
-    'P1_oppWins': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_ownChar': [torch.tensor([[7.],[7.],[7.],[7.],[7.]])],
-    'P1_ownChar1': [torch.tensor([[7.],[7.],[7.],[7.],[7.]])],
-    'P1_ownHealth': [torch.tensor([[160.],[160.],[160.],[160.],[160.]])],
-    'P1_ownSide': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_ownStunBar': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_ownStunned': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_ownSuperBar': [torch.tensor([[0.],[0.],[2.],[2.],[2.]])],
-    'P1_ownSuperCount': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'P1_ownSuperMaxCount': [torch.tensor([[3.],[3.],[3.],[3.],[3.]])],
-    'P1_ownSuperType': [torch.tensor([[2.],[2.],[2.],[2.],[2.]])],
-    'P1_ownWins': [torch.tensor([[0.],[0.],[0.],[0.],[0.]])],
-    'frame': [torch.tensor(([random.uniform(0.,255.) for _ in range(3*244*384)]))],
-    'stage': [torch.tensor([[1.],[1.],[1.],[1.],[1.]])]},
-    'actions':[torch.tensor([[6., 8.],[1., 2.],[1., 7.],[4., 5.],[1., 7.]])],
-    'old_values':[torch.tensor([-0.1244, -0.0768, -0.0817, -0.1098, -0.0945])],
-    'old_log_prob':[torch.tensor([-4.5106, -4.5129, -4.5209, -4.4947, -4.5208])],
-    'advantages':[torch.tensor([ 0.0357, -0.0128, -0.0088,  0.0184,  0.0021])],
-    'returns':[torch.tensor([-0.0887, -0.0896, -0.0905, -0.0914, -0.0923])]}
+# obs= {
+#     'observations': {
+#     'P1_actions_attack': torch.tensor([[0.],[8.],[2.],[7.],[5.]]),
+#     'P1_actions_move': torch.tensor([[0.],[6.],[1.],[1.],[4.]]),
+#     'P1_oppChar': torch.tensor([[9.],[9.],[9.],[9.],[9.]]),
+#     'P1_oppChar1': torch.tensor([[9.],[9.],[9.],[9.],[9.]]),
+#     'P1_oppHealth': torch.tensor([[160.],[160.],[160.],[160.],[160.]]),
+#     'P1_oppSide': torch.tensor([[1.],[1.],[1.],[1.],[1.]]),
+#     'P1_oppStunBar': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_oppStunned': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_oppSuperBar': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_oppSuperCount': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_oppSuperMaxCount': torch.tensor([[1.],[1.],[1.],[1.],[1.]]),
+#     'P1_oppSuperType': torch.tensor([[1.],[1.],[1.],[1.],[1.]]),
+#     'P1_oppWins': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_ownChar': torch.tensor([[7.],[7.],[7.],[7.],[7.]]),
+#     'P1_ownChar1': torch.tensor([[7.],[7.],[7.],[7.],[7.]]),
+#     'P1_ownHealth': torch.tensor([[160.],[160.],[160.],[160.],[160.]]),
+#     'P1_ownSide': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_ownStunBar': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_ownStunned': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_ownSuperBar': torch.tensor([[0.],[0.],[2.],[2.],[2.]]),
+#     'P1_ownSuperCount': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'P1_ownSuperMaxCount': torch.tensor([[3.],[3.],[3.],[3.],[3.]]),
+#     'P1_ownSuperType': torch.tensor([[2.],[2.],[2.],[2.],[2.]]),
+#     'P1_ownWins': torch.tensor([[0.],[0.],[0.],[0.],[0.]]),
+#     'frame': torch.tensor(([random.uniform(0.,255.) for _ in range(3*244*384)])),
+#     'stage': torch.tensor([[1.],[1.],[1.],[1.],[1.]])},
+#     'actions':torch.tensor([[6., 8.],[1., 2.],[1., 7.],[4., 5.],[1., 7.]]),
+#     'old_values':torch.tensor([-0.1244, -0.0768, -0.0817, -0.1098, -0.0945]),
+#     'old_log_prob':torch.tensor([-4.5106, -4.5129, -4.5209, -4.4947, -4.5208]),
+#     'advantages':torch.tensor([ 0.0357, -0.0128, -0.0088,  0.0184,  0.0021]),
+#     'returns':torch.tensor([-0.0887, -0.0896, -0.0905, -0.0914, -0.0923])}
 
-
-# env_settings_json = json.dumps(env_settings)
-# wrappers_settings_json = json.dumps(wrappers_settings)
+# reward = float
 
 result = {"env_settings" : env_settings,
-          "wrappers_settings_json" : wrappers_settings,
-          "observations" : obs}
+          "wrappers_settings" : wrappers_settings,
+        #   "observations" : obs
+          }
 
 result_json = json.dumps(result)
 
@@ -92,6 +91,22 @@ print("Write successful")
 
 # #changer .json en .py
 
-# with open("settings.json", "r") as jsonfile:
-#     data = json.load(jsonfile)
-# print(data)
+def json_to_py_start(filename):
+    with open("settings.json", "r") as jsonfile:
+
+        data = json.load(jsonfile)
+        env_settings = data["env_settings"]
+        wrapper_settings = data["wrappers_settings"]
+
+    # 2 premiers dicts settings & wrapper_settings
+    return env_settings,wrapper_settings
+
+
+# def json_to_py_agent(filename):
+#     with open("settings.json", "r") as jsonfile:
+
+#         data = json.load(jsonfile)
+#         observation = data["observations"]
+
+#     # dernier dict observations
+#     return observation
