84f91241f5185c250245d690d9f841288265c82d
Adrien Cellario <adriencellario@gmail.com>
2022-11-29 14:33:22 +0100

    push branch JynxZz

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,7 +3,28 @@ import pickle
 import json
 import os
 
-TMP_PATH="../bucket-tmp/weight.pickle"
+
+BUCKET_PATH="../bucket-tmp"
+FILE_NAME="unknow.pickle"
+
+
+def save(file_name=FILE_NAME):
+        weights_folder_path = BUCKET_PATH
+        if not os.path.exists(weights_folder_path):
+            os.makedirs(weights_folder_path)
+
+        file_name = os.path.join(weights_folder_path, file_name)
+        # torch.save(self.state_dict(), file_name)
+
+
+# TODO : Connect to the bucket
+
+# TODO : Load weigts inside bucket
+
+# TODO : Connect to the VM
+
+# TODO : Communication between VM & bucket
+
 
 if __name__ == '__main__':
      pass
