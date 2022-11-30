5e9631e6d469b2d3d245dca6296ae7ed25f0772a
Adrien Cellario <adriencellario@gmail.com>
2022-11-30 18:04:10 +0100

    main loop

--- scripts/main.py
+++ scripts/main.py
@@ -1,82 +1,79 @@
-# Import
+import diambra
 import diambra.arena
-import pandas as pd
-from random import randrange
-from scripts.config import json_to_py_start
+from diambra.arena.stable_baselines3.make_sb3_env import make_sb3_env
+
+from scripts.config import *
+from scripts.utils import *
+from scripts.agent import *
+
+from time import sleep
+
+# Load Settings & variables from config
+
+env_settings, wrappers_settings = json_to_py_start()
+
+# is_server = is_server
+# n_steps = n_steps
+# looping = looping
+
+# server_wait_time = server_wait_time
+# client_wait_time = client_wait_time
 
 
-# Environment set and observation reset
-env_settings, wrappers_settings=json_to_py_start()
-env = diambra.arena.make("sfiii3n", env_settings=env_settings,wrappers_settings=wrappers_settings)
-observation = env.reset()
-observation_all=[]
-reward_all=[]
-done_all=[]
-info_all=[]
+# Instance Env
+env, _ = make_sb3_env("sfiii3n", env_settings, wrappers_settings)
 
 
-# Agent-Environment interaction loop
-while  len(reward_all) <= 1000:
+# Instance Agent
+if is_server:
+    # n_steps => 3 x n_steps
+    server = AgentServer(env, n_steps=3 * n_steps)
 
-    # Environment rendering --> mode = "human","rgb_array"
-    env.render(mode="human")
+else:
+    client = AgentClient(env, n_steps=n_steps)
 
-    # Action random sampling // to-do plug l'AGENT
-    actions= randrange(9,15)
 
-    # Environment stepping
-    observation, reward, done, info = env.step(actions)
+# LOOP
+i = 0
+while i < looping:
+    i += 1
 
-    # Environment visualization
-    print("-----observation-----")
-    print(observation)
-    print("-----reward----------")
-    print(reward)
-    print("-----done------------")
-    print(done)
-    print("-----info------------")
-    print(info)
-    print("-----step------------")
-    print(len(reward_all)+1)
+    if server:
+        while True:
+            sleep(server_wait_time)
+            init_timestamp, is_done = switch(server.init_timestamp)
 
-    # Environment manual recording
-    observation_all.append(observation)
-    reward_all.append(reward)
-    done_all.append(done)
-    info_all.append(info)
+            if is_done:
+                buffer_1 = bucket_load("agent_one_obs.pickle")
+                # buffer_2 = bucket_load("agent_two_obs.pickle")
+                # buffer_3 = bucket_load("agent_three_obs.pickle")
 
-    # In case, Ryu wins the game --> restart a tournament without braking
-    if done:
-        env = diambra.arena.make("sfiii3n", env_settings=env_settings,wrappers_settings=wrappers_settings)
-        env.render(mode="human")
-        actions= randrange(9,11)
-        observation, reward, done, info = env.step(actions)
+                # TODO : Concat when more than 1 client
+                # concat_buffer()
 
-            # Environment visualization
-        print("-----observation-----")
-        print(observation)
-        print("-----reward----------")
-        print(reward)
-        print("-----done------------")
-        print(done)
-        print("-----info------------")
-        print(info)
-        print("-----step------------")
-        print(len(reward_all)+1)
+                server.compute(buffer_1, "new_weights")
 
-        # Environment manual recording
-        observation_all.append(observation)
-        reward_all.append(reward)
-        done_all.append(done)
-        info_all.append(info)
+                bucket_save("new_weights.zip")
 
-# Save and csv
-outputs=pd.DataFrame({
-    "observation":observation_all,
-    "reward":reward_all,
-    "done":done_all,
-    "info":info_all})
-outputs.to_csv('outputs.csv')
+                #TODO : Code evaluate method
+                #server.evaluate()
 
-# Environment close
-env.close()
+                break
+
+    if client:
+        while True:
+            client.game()
+
+            client.write_buffer("agent_one_obs.pickle") # WIP : Variables
+
+            bucket_save("agent_one_obs.pickle")
+
+            while True:
+                sleep(client_wait_time)
+                init_timestamp, is_done = switch(client.init_timestamp)
+
+                if is_done:
+                    new_weights = bucket_load("new_weights.zip")
+
+                    client.new_weights(f"{new_weights[:-4]}")
+                    break
